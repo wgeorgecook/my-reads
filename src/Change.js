8f30fb0861d9b9c4d16405cb71559f7768834323
William George Cook <wgeorgecook@gmail.com>
2018-09-15 20:45:15 -0700

    Changing shelves works but doesn't update the view immediately

--- src/Change.js
+++ src/Change.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
 
 class Change extends Component {
 
-    changeShelf = (event) => {
-        console.log(event.target.value)
+    registerChange = (e) => {
+        // console.log(e.target.value)
+        this.changeShelf(e.target.value)
+    }
+
+    changeShelf(value) {
+        // console.log(this.props.book.shelf)
+        // this.props.book.shelf = value
+        BooksAPI.update(this.props.book, value)
+        .then( () => console.log(`${this.props.book.title} moved to ${value}`))
     }
 
     render() {
         return (
             <div className="book-shelf-changer">
-                <select onChange={this.changeShelf}>
+                <select onChange={this.registerChange}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
