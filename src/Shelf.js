28596b14fc9460bd2a43e2a8919a803b92a56a39
William George Cook <wgeorgecook@gmail.com>
2018-09-15 17:29:13 -0700

    Set spacing on the changeButton variable to be proper

--- src/Shelf.js
+++ src/Shelf.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 class Shelf extends Component {
 
     changeButton =
-    <div className="book-shelf-changer">
-      <select>
-        <option value="move" disabled>Move to...</option>
-        <option value="currentlyReading">Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="read">Read</option>
-        <option value="none">None</option>
-      </select>
-    </div>
+        <div className="book-shelf-changer">
+        <select>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+        </select>
+        </div>
 
     render() {
         return (
