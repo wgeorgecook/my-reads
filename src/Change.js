8439e2957960cacdfc7c3a4c764bc777ecfc152b
William George Cook <wgeorgecook@gmail.com>
2018-09-15 19:12:03 -0700

    Change button has an on change method to get the value of the shelf we're changing to

--- src/Change.js
+++ src/Change.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 
 class Change extends Component {
 
+    changeShelf = (event) => {
+        console.log(event.target.value)
+    }
+
     render() {
         return (
             <div className="book-shelf-changer">
-                <select>
+                <select onChange={this.changeShelf}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
