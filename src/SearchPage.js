dfdba366014b981c13401b5ce925849f878f3e62
William George Cook <wgeorgecook@gmail.com>
2018-09-15 20:46:41 -0700

    Passing props to Change component on SearchPage component now

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -60,7 +60,7 @@ class SearchBar extends Component {
                     <div className="book">
                       <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-                        <Change />
+                        <Change book={book}/>
                       </div>
                       <div className="book-title">{book.title}</div>
                       <div className="book-authors">{book.authors}</div>
