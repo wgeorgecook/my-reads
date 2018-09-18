e12f172a6d1d0439ce473619131a33ee7a889866
William George Cook <wgeorgecook@gmail.com>
2018-09-17 21:51:34 -0700

    Book authors now comma separated on display

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,7 @@ class Book extends Component {
 
             </div>
             <div className="book-title">{this.props.book.title}</div>
-            <div className="book-authors">{this.props.book.authors}</div>
+            <div className="book-authors">{this.props.book.authors ? this.props.book.authors.join(", ") : ''}</div>
           </div>
         )
     }
