1ace6ee37a09ec6449ca311efb6bcc29ae30b2de
William George Cook <wgeorgecook@gmail.com>
2018-09-17 22:14:13 -0700

    A 404 page and some quick styles

--- src/FourOhFour.js
+++ src/FourOhFour.js
@@ -5,7 +5,10 @@ class NotFound extends Component {
 
     render() {
         return (
-            <div class="404">Oh no! There's nothing here. You likely took a wrong turn. Please <Link to='/'>head back home!</Link></div>
+            <div class="four-oh-four">
+                <span id='top-404'>Oh no!</span>
+                <p>There's nothing here. You likely took a wrong turn. Please <Link to='/'>head back home!</Link></p>
+            </div>
         )
     }
 }
