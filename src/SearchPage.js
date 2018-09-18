bffde31d3c9a1ba592b1a6d23d3c61e3a4f1d436
William George Cook <wgeorgecook@gmail.com>
2018-09-17 18:02:49 -0700

    Books load into search view but cannot change

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
-import Change from './Change.js'
+import Book from './Book.js'
 
 
 class SearchBar extends Component {
@@ -64,20 +64,10 @@ class SearchBar extends Component {
               { (this.state.availableBooks.length > 0) ?
                 this.state.availableBooks.map( (book) =>
                   <li key={book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                      { (book.imageLinks) ?
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div> :
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(https://www.google.com/search?q=no+image&tbm=isch&source=iu&ictx=1&fir=zK3_S7lOQ0LI6M%253A%252C029W-ajBtZqZzM%252C_&usg=AFrqEzclmm0bX0EC_BHoSGDZD9in3lmmHg&sa=X&ved=2ahUKEwjwxdTx277dAhUTO30KHXXyCwQQ9QEwAHoECAUQBA#)` }}></div>
-                      }
-                        <Change
-                            book={book}
-                            onChangeShelf={this.updateShelf}
-                        />
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                    </div>
+                  <Book
+                    book={book}
+                    onChange={this.updateShelf}
+                  />
                   </li>
               ) : null }
                 </ol>
